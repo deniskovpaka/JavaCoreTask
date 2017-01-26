41f53e6112d59162d07c227748be17892a9f888c
deniskovpaka <deniskovpaka@yandex.ru>
2017-01-26 22:58:46 +0200

    Added some logic to ProgramExecutionCore file

--- src/ProgramExecutionCore.java
+++ src/ProgramExecutionCore.java
@@ -1,30 +1,79 @@
-import model.carriages.Carriage;
-import model.carriages.PassengerCarriage;
-import model.characteristics.ComfortLevel;
-import model.characteristics.EngineType;
-import model.trains.PassengerTrain;
+import model.ResourceManager;
 import model.trains.Train;
 
-import static io.FilePaths.DATA_READER_PATH;
-import static io.FilePaths.DATA_SAVER_PATH;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ProgramExecutionCore {
+    final static Logger logger = Logger.getLogger(ProgramExecutionCore.class.getName());
     public static void main(String[] args) {
-        // TODO This execution method will be completely reworked later
-        Train train = new PassengerTrain();
-        train.setEngineType(EngineType.COAL);
-        train.addCarrage(new PassengerCarriage(ComfortLevel.COMFORT, 5.0));
-        train.addCarrage(new PassengerCarriage(ComfortLevel.LUXURY, 10.0));
-        train.addCarrage(new PassengerCarriage(ComfortLevel.LUXURY, 10.0));
-        train.addCarrage(new PassengerCarriage(ComfortLevel.STANDART, 20.0));
-        train.addCarrage(new PassengerCarriage(ComfortLevel.LUXURY, 30.0));
-        train.addCarrage(new PassengerCarriage(ComfortLevel.LUXURY, 30.0));
+        /**
+         * The args[0] argument should contain the name of
+         * file with train parameters.
+         */
+        if (args.length > 0) {
+            String fileNameWithTrainParameters = args[0];
+            ResourceManager resourceManager = new ResourceManager();
+            Train train = null;
+            try {
+                /**
+                 * Create specific train from file.
+                 */
+                train = resourceManager.createTrainFromFile(fileNameWithTrainParameters);
 
-        int passengers = 10;
-        for (Carriage passengerCarriage : train.getCarriagesCopy()) {
-            passengers += passengers;
-            ((PassengerCarriage)passengerCarriage).setNumberOfPassengers(passengers);
-        }
+                /**
+                 * Execute program till a client won't stop it.
+                 */
+                boolean isProgramExecutes = true;
+                int userInput;
+                while (isProgramExecutes) {
+                    /**
+                     * Show interface menu.
+                     */
+                    showInterfaceMenu();
+                    userInput = parseUserInput();
+                    switch (userInput) {
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            resourceManager.saveTrainToFile(createUniqueString()
+                                    + fileNameWithTrainParameters, train);
+                            break;
+                        case 5:
+                            isProgramExecutes = false;
+                            break;
+                    }
+                }
+            } catch (IOException e) {
+                logger.log(Level.WARNING, "The create train from file "
+                        + fileNameWithTrainParameters + " is FAILED!!!");
+                e.printStackTrace();
+            }
+
+
+//            // TODO This execution method will be completely reworked later
+//            Train train = new PassengerTrain();
+//            train.setEngineType(EngineType.COAL);
+//            train.addCarrage(new PassengerCarriage(ComfortLevel.COMFORT, 5.0));
+//            train.addCarrage(new PassengerCarriage(ComfortLevel.LUXURY, 10.0));
+//            train.addCarrage(new PassengerCarriage(ComfortLevel.LUXURY, 10.0));
+//            train.addCarrage(new PassengerCarriage(ComfortLevel.STANDART, 20.0));
+//            train.addCarrage(new PassengerCarriage(ComfortLevel.LUXURY, 30.0));
+//            train.addCarrage(new PassengerCarriage(ComfortLevel.LUXURY, 30.0));
+//
+//            int passengers = 10;
+//            for (Carriage passengerCarriage : train.getCarriagesCopy()) {
+//                passengers += passengers;
+//                ((PassengerCarriage) passengerCarriage).setNumberOfPassengers(passengers);
+//            }
 //        System.out.println("Passengers=" + passengers);
 //        System.out.println("------BEFORE------");
 //        System.out.println(train.getCarriagesCopy());
@@ -45,14 +94,48 @@ public class ProgramExecutionCore {
 //        TxtDataReader reader = new TxtDataReader(DATA_READER_PATH + "PassengerTrain.txt");
 //        TxtDataSaver saver = new TxtDataSaver(DATA_SAVER_PATH + "PassengerTrainOut.txt");
 //        XmlDataReader reader = new XmlDataReader(DATA_READER_PATH + "PassengerTrain.xml");
-        // XmlDataSaver saver = new XmlDataSaver(DATA_SAVER_PATH + "PassengerTrainOut.xml");
-        //saver.saveDataToFile(train);
-        //saver.saveDataToFile(train);
+            // XmlDataSaver saver = new XmlDataSaver(DATA_SAVER_PATH + "PassengerTrainOut.xml");
+            //saver.saveDataToFile(train);
+            //saver.saveDataToFile(train);
 //        String[] str = reader.readInitialParameters();
 //        for (String s : str)
 //            System.out.println(s);
-        System.out.println(DATA_SAVER_PATH);
-        System.out.println(DATA_READER_PATH);
-        // TODO This execution method will be completely reworked later
+//            System.out.println(DATA_SAVER_PATH);
+//            System.out.println(DATA_READER_PATH);
+            // TODO This execution method will be completely reworked later
+        }
+    }
+
+    private static void showInterfaceMenu() {
+        System.out.println("What would you like to do with train:");
+        System.out.println("1. Sort by specific parameter in carriages");
+        System.out.println("2. Calculate by specific parameter in carriages");
+        System.out.println("3. Find/Filtered Out by specific parameter in carriages");
+        System.out.println("4. Save train to file");
+        System.out.println("5. Exit from program");
+    }
+
+    private static int parseUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        String userInput;
+        int input;
+        do {
+            System.out.println("Please make your choice)))");
+            userInput = scanner.nextLine();
+            try {
+                input = Integer.parseInt(userInput);
+            } catch (NumberFormatException nfe) {
+                System.out.println("Your input is INCORRECT!!!\r\n");
+                input = 0;
+            }
+        } while (input < 1 || input > 5);
+        System.out.println("The client input is " + input);
+        return input;
+    }
+
+    private static String createUniqueString() {
+        Calendar cal = Calendar.getInstance();
+        return new SimpleDateFormat("EEEE, dd/MM/yyyy/hh:mm:ss")
+                                    .format(cal.getTime());
     }
 }
